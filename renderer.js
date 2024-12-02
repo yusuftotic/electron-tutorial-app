93055af98635e809c598345cd1e2aef485fa6152
yusuftotic <yusuftotic@outlook.com>
2024-12-02 16:53:06 +0300

    Using Preload Scripts

---
+++ renderer.js
@@ -0,0 +1,10 @@
+const information = document.getElementById('info')
+
+information.innerText = `This app is using Chrome v${versions.chrome} Node v${versions.node} Electron v${versions.electron}`
+
+const func = async () => {
+  const response = await window.versions.ping()
+  console.log(response) // prints out 'pong'
+}
+
+func()
