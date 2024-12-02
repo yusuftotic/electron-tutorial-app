a361e63c5903aae8f2fcb3873b6197cf5e80e0e3
yusuftotic <yusuftotic@outlook.com>
2024-12-02 17:51:16 +0300

    Publishing and Updating

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'yusuftotic',
+          name: 'electron-tutorial-app'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 
 const path = require('node:path')
 
+require('update-electron-app')()
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
